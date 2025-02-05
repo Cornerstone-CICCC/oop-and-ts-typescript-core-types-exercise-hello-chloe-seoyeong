0a90e0ac4173b49d84ef147f3eebccd2f638d112
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-05 12:26:19 -0800

    Completed exercise 03

--- src/ex03.ts
+++ src/ex03.ts
@@ -1,8 +1,19 @@
 // Exercise: Write a function `logValues` that takes an array of any type of values (numbers, strings, booleans, etc.).
 // The function should iterate through the array and log each value along with its type.
 
-function logValues(values) {
 
+
+function logValues(values: any[]) {
+  values.forEach(value => {
+    if(typeof value === "object") {
+      console.log(`${JSON.stringify(value)} (${typeof value})`)
+    } else if(typeof value === "string") {
+      console.log(`"${value}" (${typeof value})`)
+    }else {
+      console.log(`${value} (${typeof value})`)
+    }
+
+  })
 }
 
 logValues([42, "Hello", true, { name: "Alice" }]);
